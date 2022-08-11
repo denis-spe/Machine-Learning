1bc335f6d874fb2f67847dc643e77a2645468594
denis-spe <denisbrian07@gmail.com>
2022-08-11 02:43:30 +0200

    Added bar charts from missing values

--- pages/2_Data_Cleaning.py
+++ pages/2_Data_Cleaning.py
@@ -1,6 +1,8 @@
 # import libraries ------------------------------------
+from cmath import nan
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 # Set the page main title
 st.set_page_config(
@@ -11,6 +13,36 @@ st.set_page_config(
 def load_css(path):
     with open(path, mode='r') as file:
         st.markdown(f"<style>{file.read()}</style>", unsafe_allow_html=True)
+
+
+def nan_check(data: pd.DataFrame):
+    """
+    Check for missing values in the data frame.
+    
+    # Parameter:
+
+        data: (pd.DataFrame)  data to check for missing values.
+
+    # Return:  
+        
+        data_missing_df: Dataframe with number of missing data and precent.
+    """
+    # Get the missing values with 'isna' method.
+    data_missing = data.isna().sum()
+
+    # Calculate the precentage of missing values.
+    precent = np.array(list(
+        map(lambda x: str(x) + "%", list(np.round(data_missing / data.shape[0], 3) * 100))
+        ))
+
+    # Construct a missing values data frame.
+    data_missing_df = pd.DataFrame({
+        "Col": data.columns,
+        "N_Missing_Values": data_missing,
+        "Percent": precent
+        }).reset_index(drop=True)
+    
+    return data_missing_df
 
 
 # loading the css file 
@@ -73,14 +105,21 @@ if "train_X" in session:
     # Make a select box to select columns to fill.
     col_to_fill = SIDEBAR.multiselect(
         label="Select columns to fill", 
-        options=train_X.columns
+        options=train_X.columns,
         )
+
+    methods = [0, 'mean', 'median', 'mode', 'unknown'] * len(train_X.columns)
+
+    def add_back_opt(x):
+        methods.append(x)
+        return x
 
     # A Select box to be used to fill missing values in the column.
     fill_options = SIDEBAR.multiselect(
         label="Options to be used to fill", 
-        options=[0, 'mean', 'median', 'mode', 'unknown']
+        options= methods,
         )
+
 
     # Loop over a zip of col_to_fill with fill_options
     for col, fill in zip(col_to_fill, fill_options):
@@ -124,12 +163,22 @@ if "train_X" in session:
             except NameError:
                 pass
         
+    # Create bar chart from showing the number missing values in each column .........
+    col1.bar_chart(train_X.isna().sum())
+    col2.bar_chart(valid_X.isna().sum())
+    try:
+        col4.bar_chart(test.isna().sum())
+    except NameError:
+        pass
 
     # Display dataframes .......................
     with col1.expander("Train Data"):
 
+        # Construct a missing values data frame.
+        train_X_missing_df = nan_check(train_X)
+
         # Display the dataframe with number of missing values.
-        st.write(train_X.isna().sum().rename("N_NaN"))
+        st.write(train_X_missing_df)
 
         # Check if the target variable contains missing values.
         st.write(f"Number of NaN in {session.target_name}: {train_y.isna().sum()}")
@@ -140,8 +189,12 @@ if "train_X" in session:
         st.write(train_X)
 
     with col2.expander("Validation Data"):
+
+        # Construct a missing values data frame.
+        valid_X_missing_df = nan_check(valid_X)
+
         # Display the dataframe with number of missing values.
-        st.write(valid_X.isna().sum().rename("N_NaN"))
+        st.write(valid_X_missing_df)
 
         # Check if the target variable contains missing values.
         st.write(f"Number of NaN in {session.target_name}: {valid_y.isna().sum()}")
@@ -154,8 +207,11 @@ if "train_X" in session:
     if "test" in session:
         with col4.expander("Test Data"):
 
+            # Construct a missing values data frame.
+            test_X_missing_df = nan_check(test)
+
             # Display the dataframe with number of missing values.
-            st.write(test.isna().sum().rename("N_NaN"))
+            st.write(test_X_missing_df)
 
             # Add a dataframe for inspection
             st.write('---')
